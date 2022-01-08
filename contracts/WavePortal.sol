e465c42e48233050ac0f0d295fb666867214df2f
AmitrajitDas <amitrajitdas31@gmail.com>
2022-01-08 17:09:04 +0530

    feat: waveportal smart contract init

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+}
---
+++ scripts/run.js
@@ -0,0 +1,18 @@
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal')
+  const waveContract = await waveContractFactory.deploy()
+  await waveContract.deployed()
+  console.log('Contract deployed to:', waveContract.address)
+}
+
+const runMain = async () => {
+  try {
+    await main()
+    process.exit(0)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+runMain()
